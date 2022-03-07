4fe7789642161e3209cc3c0e1f6dc0ee565e2334
bohan1109 <4A790162@stust.edu.tw>
2022-03-07 11:49:50 +0800

    test sql3

--- database/migrations/2022_02_25_085304_create_registions_table.php
+++ database/migrations/2022_02_25_085304_create_registions_table.php
@@ -37,6 +37,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('registions');
+        Schema::dropIfExists('registrations');
     }
 };
